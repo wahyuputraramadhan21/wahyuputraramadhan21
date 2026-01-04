a419feb339cccdf1bece638c8bd7a55b3f41add2
wahyuputraramadhan21 <100779277+wahyuputraramadhan21@users.noreply.github.com>
2026-01-04 13:47:20 +0700

    Update SCRIPT_URL for Google script endpoint

--- api/submit.js
+++ api/submit.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwtL95b5VtkbFM334ppe0jgL2_NW3UbxajFJgl9Uhrvkqsl3MiZ3OKE9eVS4eK8SUbj/exec";
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoBKIkFDutM519A8UXEHB9nrgMwVAbVsTtqSx9mQmYMkX6nEbWA8pvOXtkZHoshVq5/exec";
 
   try {
     if (req.method === 'POST') {
